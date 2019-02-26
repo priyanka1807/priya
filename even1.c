6d14f6bdf94ad515d89db4a0df89425bb8092839
priyanka1807 <46019698+priyanka1807@users.noreply.github.com>
2019-02-26 12:13:47 +0530

    Create even1.c

---
+++ even1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{  
+int counter; 
+printf("Even numbers between 7 to 9 \n");  
+for(counter = 7; counter <= 9; counter++) 
+{  
+printf("%d ", counter);  
+}  
+}  
+getch();   
+   
+} 
