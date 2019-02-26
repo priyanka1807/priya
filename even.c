e5233b28fb0b646e5bf6b466a9c21a344352bce9
priyanka1807 <46019698+priyanka1807@users.noreply.github.com>
2019-02-26 12:11:31 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{  
+int counter; 
+printf("Even numbers between 1 to 100\n");  
+for(counter = 1; counter <= 100; counter++) {  
+printf("%d ", counter);  
+}  
+}  
+getch();   
+   
+} 
