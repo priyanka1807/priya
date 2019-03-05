b13878f22626fb20cae314f27ea3ed3f03119f32
priyanka1807 <46019698+priyanka1807@users.noreply.github.com>
2019-03-05 12:22:51 +0530

    Update and rename factorial.c to 14.c

---
+++ 14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#includ<conio.h>
+void main()
+{
+int fact=1,num;
+printf("Enter the Number :");
+for(i=0;i<num;i++)
+{
+fact=fact*num;
+}
+printf("The Factorial of number is :",fact);
+}
